f0d3e458f5e2ff179624e73af5c479bafe699ce0
Hugo Eseverri <hugooeseverri@gmail.com>
2025-07-14 02:26:12 -0300

    cambios

--- invitacion/src/app/enviado/page.tsx
+++ invitacion/src/app/enviado/page.tsx
@@ -46,6 +46,12 @@ export default function Enviado() {
                 >
                     ¡Gracias por elegir ser parte de una noche tan especial!
                 </h2>
+                <h2
+                    className="text-2xl mb-4 mt-20 tracking-wider w-[350px] text-center leading-[30px] text-white"
+                    style={{ fontFamily: "var(--font-playfair)" }}
+                >
+                    ¡Ahora vas a ser redirigida/o a Whatsapp, nos ayudarías confirmando también con un mensaje!
+                </h2>
                 <Image
                     src="/retroflower3.PNG"
                     width={300}
